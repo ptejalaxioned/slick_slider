9dfe33606975149a121de49babe98de59f7a98e4
ptejalaxioned <tejalp@axioned.com>
2024-06-20 15:29:24 +0530

    js file commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,87 @@
 $(document).ready(function(){
-    $('.your-slider').slick({
+    $('.slider').slick({
         dots: true,
         arrows: true,
+        centerMode: true,
         infinite: true,
         speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        fade: false,
+        cssEase: 'linear',
+        prevArrow: $('.custom-prev-arrow'),
+        nextArrow: $('.custom-next-arrow'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    vertical:true,
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 995,
+                settings: {
+                    dots: true,
+                    arrows: true,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 776,
+                settings: {
+                    dots: true,
+                    arrows: true,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    dots: true,
+                    arrows: true,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    vertical:true,
+                    centerMode: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: false,
+                    arrows: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    vertical:true,
+                    centerMode: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: false,
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
     });
 });
-
 
 
 
