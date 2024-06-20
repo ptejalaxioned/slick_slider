9b0b22ae0437c9ce65f31dcbd84a0ad63a416d54
ptejalaxioned <tejalp@axioned.com>
2024-06-20 11:40:28 +0530

    js file commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
-
+$(document).ready(function(){
+    $('.your-slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
 
 
 
